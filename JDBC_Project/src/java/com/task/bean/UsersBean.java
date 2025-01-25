b20c03e0ef017975773c8bd900ebd67c2d4f1e30
Aatmik-06 <adi120kush@gmail.com>
2025-01-26 00:07:10 +0530

    UserBean updated and form UI Updated

--- JDBC_Project/src/java/com/task/bean/UsersBean.java
+++ JDBC_Project/src/java/com/task/bean/UsersBean.java
@@ -10,4 +10,82 @@ package com.task.bean;
  */
 public class UsersBean {
     
+    private int Uid;
+    private String Name;
+    private String UserName;
+    private String Password;
+    private String Email;
+    private String Mobile;
+
+    //    making default constructor 
+    public UsersBean() {
+    }
+    
+    //    making parametered constructor 
+
+    public UsersBean(int Uid, String Name, String UserName, String Password, String Email, String Mobile) {
+        this.Uid = Uid;
+        this.Name = Name;
+        this.UserName = UserName;
+        this.Password = Password;
+        this.Email = Email;
+        this.Mobile = Mobile;
+    }
+    
+    
+//    Getter/Setter
+
+    public int getUid() {
+        return Uid;
+    }
+
+    public void setUid(int Uid) {
+        this.Uid = Uid;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String UserName) {
+        this.UserName = UserName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getMobile() {
+        return Mobile;
+    }
+
+    public void setMobile(String Mobile) {
+        this.Mobile = Mobile;
+    }
+    
+    
+    
+    
+    
+    
 }
