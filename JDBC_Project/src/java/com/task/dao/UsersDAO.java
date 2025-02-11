2391637391d594f30fc859254d7d658d053e323e
Aatmik-06 <adi120kush@gmail.com>
2025-02-11 14:25:53 +0530

    updated

--- JDBC_Project/src/java/com/task/dao/UsersDAO.java
+++ JDBC_Project/src/java/com/task/dao/UsersDAO.java
@@ -4,10 +4,58 @@
  */
 package com.task.dao;
 
+import com.task.utility.ConnectionPool;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import com.task.bean.UsersBean;
+
 /**
  *
  * @author adi18
  */
+
 public class UsersDAO {
+    static Connection conn;
+    public int registerUser(UsersBean ub){
+        conn= ConnectionPool.connectDB();
+         String sql="insert into users values('"+ub.getUid()+"','"+ub.getName()+"','"+ub.getUserName()+"','"+ub.getPassword()+"','"+ub.getEmail()+"','"+ub.getMobile()+"')";
+        //step4:
+        Statement stmt;
+        int r=0;
+        try {
+            stmt = conn.createStatement();
+            r=stmt.executeUpdate(sql);
+              //step6:
+        conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(UsersDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+             
+        return r;
+    }
     
-}
+     public int loginCheck(String username,String password){
+          conn= ConnectionPool.connectDB();
+          int id = 0 ;
+         String sql="select uid from users where username ='"+username+"' and password='"+password+"'";
+        //step4:
+        Statement stmt;
+        try {
+            stmt = conn.createStatement();
+            ResultSet rs =stmt.executeQuery(sql);
+              //step6:
+        conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(UsersDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+             
+        return id;
+     }
+     public static void main(String[] args) {
+        
+    }
+   }
